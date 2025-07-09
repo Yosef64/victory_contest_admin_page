1a86d4b09c78d73ab349111589a8f4b99b84dd64
HermelaDemeke <bhmd0595@gmail.com>
2025-07-09 21:17:05 +0300

    questionpage : editing question functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -59,5 +61,7 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    tailwindcssAnimate
+  ],
 };

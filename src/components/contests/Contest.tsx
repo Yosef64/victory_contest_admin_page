4702799a8b265fe3a3cbc91b15ec4d03c6462897
Yosef64 <yosefale65@gmail.com>
2025-08-11 23:21:35 +0300

    remvong warnings

--- src/components/contests/Contest.tsx
+++ src/components/contests/Contest.tsx
@@ -1,5 +1,5 @@
 // src/pages/ContestPage.tsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
